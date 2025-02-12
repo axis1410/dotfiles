62acddfac87a1ff668b12fb0ddb2986e43393382
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-12 21:12:46 +0530

    add nvim and wezterm configs

---
+++ .wezterm.lua
@@ -0,0 +1,55 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config = {
+	automatically_reload_config = true,
+	enable_tab_bar = false,
+	window_close_confirmation = "NeverPrompt",
+	font = wezterm.font("JetBrainsMono Nerd Font Mono", { weight = "Regular" }),
+	font_size = 15,
+	window_decorations = "RESIZE",
+	color_scheme = "Oxocarbon Dark (Gogh)",
+	native_macos_fullscreen_mode = true,
+	window_padding = {
+		left = 3,
+		right = 3,
+		top = 0,
+		bottom = 0,
+	},
+	keys = {
+		{
+			key = "n",
+			mods = "SHIFT|CTRL",
+			action = wezterm.action.ToggleFullScreen,
+		},
+	},
+}
+
+local function center_window_once(window)
+	wezterm.GLOBAL.windows_centered = wezterm.GLOBAL.windows_centered or {}
+	local window_id = window:window_id() .. ""
+	if wezterm.GLOBAL.windows_centered[window_id] then
+		return
+	end
+
+	local screen = wezterm.gui.screens().active
+
+	local width = screen.width * 0.75
+	local height = screen.height * 0.75
+
+	window:set_inner_size(width, height)
+
+	local dimensions = window:get_dimensions()
+	local x = (screen.width - dimensions.pixel_width) * 0.5
+	local y = (screen.height - dimensions.pixel_height) * 0.5
+
+	wezterm.GLOBAL.windows_centered[window_id] = true
+
+	window:set_position(x, y)
+end
+
+wezterm.on("update-status", function(window)
+	center_window_once(window)
+end)
+
+return config
