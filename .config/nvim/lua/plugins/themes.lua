4c1b1c9567e882abb52114a7083d843a72205431
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-12 21:09:08 +0530

    initialize dotfiles

---
+++ .config/nvim/lua/plugins/themes.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "scottmckendry/cyberdream.nvim",
+    lazy = false,
+    priority = 1000,
+  },
+  {
+    "navarasu/onedark.nvim",
+    lazy = false,
+    opts = {
+      code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+      },
+      style = "dark", -- Choose from "dark", "darker", "cool", "deep", "warm", "warmer", (do not choose) "light"
+      lualine = {
+        transparent = true,
+      },
+    },
+  },
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
+  {
+    "decaycs/decay.nvim",
+    lazy = false,
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+      indentguides = false,
+    },
+  },
+}
