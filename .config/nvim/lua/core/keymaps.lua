5eeda0f4f7fe4bcba75d1443ddd02841bd8f96a7
Aditya Singh <adityasingh14102001@gmail.com>
2025-02-13 02:32:09 +0530

    update nvim config

---
+++ .config/nvim/lua/core/keymaps.lua
@@ -0,0 +1,98 @@
+-- Set leader key
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Disable the spacebar key's default behavior in Normal and Visual modes
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- For conciseness
+local opts = { noremap = true, silent = true }
+
+-- save file
+vim.keymap.set('n', '<C-s>', '<cmd> w <CR>', opts)
+
+-- save file without auto-formatting
+vim.keymap.set('n', '<leader>sn', '<cmd>noautocmd w <CR>', opts)
+
+-- quit file
+vim.keymap.set('n', '<C-q>', '<cmd> q <CR>', opts)
+
+-- delete single character without copying into register
+vim.keymap.set('n', 'x', '"_x', opts)
+
+-- Vertical scroll and center
+vim.keymap.set('n', '<C-d>', '<C-d>zz', opts)
+vim.keymap.set('n', '<C-u>', '<C-u>zz', opts)
+
+-- Find and center
+vim.keymap.set('n', 'n', 'nzzzv', opts)
+vim.keymap.set('n', 'N', 'Nzzzv', opts)
+
+-- Resize with arrows
+vim.keymap.set('n', '<Up>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<Down>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<Left>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<Right>', ':vertical resize +2<CR>', opts)
+
+-- Buffers
+vim.keymap.set('n', '<Tab>', ':bnext<CR>', opts)
+vim.keymap.set('n', '<S-Tab>', ':bprevious<CR>', opts)
+vim.keymap.set('n', '<leader>x', ':bdelete!<CR>', opts) -- close buffer
+vim.keymap.set('n', '<leader>b', '<cmd> enew <CR>', opts) -- new buffer
+
+-- Window management
+vim.keymap.set('n', '<leader>v', '<C-w>v', opts) -- split window vertically
+vim.keymap.set('n', '<leader>h', '<C-w>s', opts) -- split window horizontally
+vim.keymap.set('n', '<leader>se', '<C-w>=', opts) -- make split windows equal width & height
+vim.keymap.set('n', '<leader>xs', ':close<CR>', opts) -- close current split window
+
+-- Navigate between splits
+vim.keymap.set('n', '<C-k>', ':wincmd k<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':wincmd j<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':wincmd h<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':wincmd l<CR>', opts)
+
+-- Tabs
+vim.keymap.set('n', '<leader>to', ':tabnew<CR>', opts) -- open new tab
+vim.keymap.set('n', '<leader>tx', ':tabclose<CR>', opts) -- close current tab
+vim.keymap.set('n', '<leader>tn', ':tabn<CR>', opts) --  go to next tab
+vim.keymap.set('n', '<leader>tp', ':tabp<CR>', opts) --  go to previous tab
+
+-- Toggle line wrapping
+vim.keymap.set('n', '<leader>lw', '<cmd>set wrap!<CR>', opts)
+
+-- Stay in indent mode
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+
+-- Keep last yanked when pasting
+vim.keymap.set('v', 'p', '"_dP', opts)
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+vim.keymap.set('n', '<leader>d', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Lazy
+vim.keymap.set("n", "<leader>l", ":Lazy<CR>", { desc = "Open Lazy", silent = true })
+
+
+-- System clipboard integration
+vim.keymap.set("n", "<leader>y", [["+y]], { desc = "yank to system clipboard" })
+
+-- Delete operations without yanking
+vim.keymap.set("n", "<leader>dd", '"_dd', { desc = "delete line without yanking" }) -- Changed from 'dd' to '<leader>dd' to preserve default 'dd'
+vim.keymap.set("v", "<leader>d", '"_d', { desc = "delete selection without yanking" }) -- Changed from 'dd' to '<leader>d' in visual mode
+vim.keymap.set("n", "<leader>dw", '"_diw', { desc = "delete inner word without yanking" }) -- Changed from 'diw' to '<leader>dw'
+
+-- Delete all lines without yanking
+vim.keymap.set("n", "<leader>da", 'gg0vg$"_d', { desc = "delete entire file's code without yanking" }) -- Changed from '<leader>d' to '<leader>da' to be more specific
+
+-- Move selected lines up/down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "move selected lines down" }) -- Changed from 'j' to 'J' to avoid breaking normal navigation
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "move selected lines up" }) -- Changed from 'k' to 'K' to avoid breaking normal navigation
+
+-- Select all text
+vim.keymap.set("n", "<leader>sa", "ggVG", { desc = "select all text" })
+         
---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+-- lua/plugins/colorscheme.lua
+return {
+    {
+        'navarasu/onedark.nvim',
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require('onedark').setup {
+                style = 'darker',
+                code_style = {
+                    comments = 'italic',
+                    keywords = 'none',
+                    functions = 'none',
+                    strings = 'none',
+                    variables = 'none'
+                },
+            }
+            vim.cmd.colorscheme 'onedark'
+        end
+    }
+}
