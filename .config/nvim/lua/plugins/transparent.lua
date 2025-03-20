47b829f9a3c4238e993076f9d16884a849cfda61
Aditya Singh <adityasingh14102001@gmail.com>
2025-03-20 13:17:35 +0530

    minor optimizations

--- .config/nvim/lua/plugins/transparent.lua
+++ .config/nvim/lua/plugins/transparent.lua
@@ -1,5 +1,13 @@
 return {
 	{
 		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				extra_groups = {
+					"NormalFloat",
+					"NvimTreeNormal",
+				},
+			})
+		end,
 	},
 }
