c68ce48513358c872cc41f13ac181a3d5d662eb3
Aditya Singh <aditya.s@navtech.io>
2025-12-15 16:06:25 +0530

    fixes and optimizations

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -68,7 +68,7 @@ vim.g.loaded_netrwPlugin = 1 -- We use our own file explorer
 -- vim.g.loaded_netrwSettings = 1
 -- vim.g.loaded_netrwFileHandlers = 1
 
--- vim.opt.colorcolumn = "120"
+vim.opt.colorcolumn = "120"
 
 vim.g.neovide_scroll_animation_length = 0
 vim.g.neovide_scroll_animation_far_lines = 0
--- .config/nvim/lua/core/theme.lua
+++ .config/nvim/lua/core/theme.lua
@@ -1,7 +1,7 @@
 local M = {}
 
 M.set_theme = function()
-    vim.cmd.colorscheme("gruvbox-material")
+    vim.cmd.colorscheme("edge")
 end
 
 return M
