3ec476583bb80ad73f39d6ca500beaeaf91e3a93
Aditya Singh <aditya.s@navtech.io>
2025-12-23 15:07:17 +0530

    update lspconfig

--- .config/nvchad/lua/configs/lspconfig.lua
+++ .config/nvchad/lua/configs/lspconfig.lua
@@ -1,8 +1,11 @@
 require("nvchad.configs.lspconfig").defaults()
 
+local lsp = vim.lsp
+
 local servers = {
   "ts_ls",
-  "pyright",
+  -- "pyright",
+  "basedpyright",
   "ruff",
   "html",
   "jinja_lsp",
@@ -15,6 +18,28 @@ local servers = {
   "rust_analyzer",
   "lua_ls",
 }
+
+lsp.config("ruff", {
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
+    callback = function(args)
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      if client == nil then
+        return
+      end
+      if client.name == "ruff" then
+        -- Disable hover in favor of Pyright
+        client.server_capabilities.hoverProvider = false
+      end
+    end,
+    desc = "LSP: Disable hover capability from Ruff",
+  }),
+
+  init_options = {
+    settings = {},
+  },
+})
+
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
