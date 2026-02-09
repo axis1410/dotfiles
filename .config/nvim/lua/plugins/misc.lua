53d1a3138407208dbdef6283fcac6e3a1d382983
Aditya Singh <aditya.s@navtech.io>
2026-02-09 12:32:30 +0530

    change todo lua's event

--- .config/nvim/lua/plugins/misc.lua
+++ .config/nvim/lua/plugins/misc.lua
@@ -2,7 +2,7 @@ return {
   {
     "folke/todo-comments.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    cmd = "TodoFzfLua",
+    event = "BufEnter",
     keys = {
       {
         "<leader>td",
