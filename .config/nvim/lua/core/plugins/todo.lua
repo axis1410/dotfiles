d8250a2bc15ea0cda6fbffdc19aa677fa1b6b349
Aditya Singh <aditya.s@navtech.io>
2026-02-18 13:45:53 +0530

    add todo.nvim

---
+++ .config/nvim/lua/core/plugins/todo.lua
@@ -0,0 +1,10 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  event = "BufEnter",
+  opts = {},
+  config = function()
+    require("todo-comments").setup {}
+    vim.keymap.set("n", "<leader>td", ":TodoFzfLua keywords=TODO,FIX<CR>", { noremap = true })
+  end,
+}
