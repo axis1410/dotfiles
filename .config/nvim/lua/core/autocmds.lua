13bdc16cff9853b9e73c236921bea95ef4968564
Aditya Singh <aditya.s@navtech.io>
2026-04-02 23:24:43 +0530

    update jb.nvim palette

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,1 +1,15 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+  pattern = "jb",
+  callback = function()
+    require("core.jb_palette").apply()
+  end,
+})
 
+vim.api.nvim_create_autocmd("OptionSet", {
+  pattern = "background",
+  callback = function()
+    if vim.g.colors_name == "jb" then
+      require("core.jb_palette").apply()
+    end
+  end,
+})
